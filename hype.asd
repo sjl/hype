31b490277b5d525b95d5dff00f58ddd47aa53848
Steve Losh <steve@stevelosh.com>
2016-07-06 18:47:01 +0000

    Initial commit

---
+++ hype.asd
@@ -0,0 +1,23 @@
+(asdf:defsystem #:hype
+  :name "hype"
+  :description "HYPE"
+
+  :author "Steve Losh <steve@stevelosh.com>"
+
+  :license "MIT/X11"
+  :version "0.0.1"
+
+  :depends-on (#:defstar
+               #:iterate
+               #:cl-arrows
+               #:cl-ggp
+               #:bones)
+
+  :serial t
+  :components
+  ((:file "quickutils") ; quickutils package ordering crap
+   (:file "package")
+   (:module "src"
+    :serial t
+    :components ((:file "utils")
+                 ))))
---
+++ package.lisp
@@ -0,0 +1,43 @@
+(defpackage #:hype.utils
+  (:use
+    #:cl
+    #:defstar
+    #:iterate
+    #:cl-arrows
+    #:hype.quickutils)
+  (:export
+    #:zap%
+    #:%
+    #:recursively
+    #:recur
+    #:dis
+
+    #:hash-set
+    #:make-set
+    #:set-contains-p
+    #:set-add
+    #:set-remove
+    #:set-add-all
+    #:set-remove-all
+    #:set-random
+    #:set-pop
+    #:set-empty-p
+    #:set-clear
+
+    #:averaging
+    #:timing
+    #:real-time
+    #:run-time
+    #:since-start-into
+    #:per-iteration-into
+
+    #:queue
+    #:queue-contents
+    #:enqueue
+    #:dequeue
+    #:queue-empty-p
+    #:queue-append
+
+    )
+  (:shadowing-import-from #:cl-arrows
+    #:->))
